a41f80b0599dcaf525fd9bdcd9377f756103b79c
venus00 <farhat.oussama00@gmail.com>
2024-05-28 17:26:39 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,10 @@ function App() {
     function onData(image: string) {
       setImg(Buffer.from(image, 'base64').toString())
     }
+    function onData2(image: string) {
+      console.log('test2');
+      // setImg(Buffer.from(image, 'base64').toString())
+    }
 
     function onConnect() {
       console.log('connected');
@@ -22,12 +26,16 @@ function App() {
     socket.on('connect', onConnect);
     socket.on('disconnect', onDisconnect);
     socket.on('test', onData);
+    socket.on('test2', onData2);
+
 
     return () => {
       socket.disconnect()
       socket.off('connect', onConnect);
       socket.off('disconnect', onDisconnect);
       socket.off('test', onData);
+      socket.off('test2', onData);
+
     };
   }, []);
 
@@ -35,6 +43,7 @@ function App() {
     <>
       <div>test stream</div>
       <img src={`data:image/jpeg;base64,${img}`} width={640} height={480} />
+      {/* <img src={`http://127.0.0.1:3002/camera1`} width={640} height={480} /> */}
     </>
   )
 }
